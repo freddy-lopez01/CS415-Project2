4eccec25b3d75f7bbdbc0e54373d51b6a9748ac8
Freddy Lopez <freddlopez01@gmail.com>
2023-05-02 12:19:16 -0700

    Pushing current packetdriver.c version

--- packetdriver.c
+++ packetdriver.c
@@ -10,13 +10,16 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <sys/wait.h>
+#include <pthread.h>
+#include <stdbool.h>
 //additional #includes needed
+#include "BoundedBuffer.h"
 #include "destination.h"
 #include "diagnostics.h"
 #include "fakeapplications.h"
-#include "freepacketdescriptorstore_full.h"
+#include "freepacketdescriptorstore__full.h"
 #include "freepacketdescriptorstore.h"
-#include "networkdevice_full.h"
+#include "networkdevice__full.h"
 #include "networkdevice.h"
 #include "packetdescriptorcreator.h"
 #include "packetdescriptor.h"
@@ -24,54 +27,124 @@
 #include "queue.h"
 
 
+#define MAX_PID 10
+#define UNUSED __attribute__((unused))
+typedef struct network_device NetworkDevice;
+unsigned long myfpds_size; 
+int count = 0; 
+int in = 0; 
+int out = 0; 
+
+pthread_t consumer_thread;
+pthread_t producer_thread;
+NetworkDevice *myNetworkDevice; 
+//bounded buffer implimentation
+BoundedBuffer *bbin = NULL; // Bounded buffer for producers packages 
+BoundedBuffer *bbout = NULL; // Bounded buffer for consumer packages 
+FreePacketDescriptorStore *myfpds; 
+PID *arr_pid[MAX_PID+1];
 
 
-typedef struct free_packet_descriptor_store FreePacketDescriptorStore;
 
-FreePacketDescriptorStore *FreePacketDescriptorStore_create(void *mem_start, unsigned long mem_length);
+static int SandSbuffer(PacketDescriptor **packetFound, PID pid){
 
-void FreePacketDescriptorStore_destroy(FreePacketDescriptorStore *fpds); 
-
-struct free_packet_descriptor_store{
-	/*
-	Blocking verison: Only returns if it is successful 
-	NonBlocking version: returns 1 if successful; 0 otherwise
-	Get Method set their final argument if successful
-	*/
-	void *self; 
-	void (*blockingGet)(FreePacketDescriptorStore *fpds, PacketDescriptor **pd);
-	int (*nonblockingGet)(FreePacketDescriptorStore *fpds, PacketDescriptor **pd);
-	void (*blockingPut)(FreePacketDescriptorStore *fpds, PacketDescriptor **pd);
-	int (*nonblockingPut)(FreePacketDescriptorStore *fpds, PacketDescriptor **pd);
-	unsigned long (*size)(FreePacketDescriptorStore *fpds); 
-
+	for(int i = 0; i < (MAX_PID+1); i++){
+		PacketDescriptor *checkpd; 
+		bbout->blockingRead(bbout, (void**)&checkpd);
+		PID checkPID = getPID(checkpd);
+		if(checkPID == pid){
+			printf("Packet found for Application %d\n", pid);
+			*packetFound = checkpd;
+			return 1; 
+		}
+		bbout->blockingWrite(bbout, checkpd);
+	}
+	return 0;
 }
 
 
-/*
-When OS boots up, invoke the following initialization routine
-Called before any other methods, to allow you to initialize data 
-structures and start any internal threads
+PacketDescriptor *currentPD; 
 
-Args:
-nd: the network device that I must drive
-mem_start, mem_length: some memory for PacketDescriptors
-fpds: You hand back a FreePacketDescriptorStore into which PacketDescriptor build from the memory found 
-and described in args 2 and 3 have been put
-*/
+void *getWork(UNUSED void *arg){
+	//PacketDescriptor *currentPD; 
+	printf("Thread created successfully\n");
+	
+	// remove packet from fpds
+	//clear pd
+	while(1){
+		myfpds->blockingGet(myfpds, (void*)&currentPD);
+		initPD(currentPD);
+		// register packet descriptor to the Network Device 
+		myNetworkDevice->registerPD(myNetworkDevice, currentPD); // what "pd" should this point to 
+		// wait for the packet descriptor to be populated 
+		myNetworkDevice->sendPacket(myNetworkDevice, currentPD);
+
+		
+
+		printf("AwaitngIncomingPacket....\n");
+		myfpds_size = myfpds->size(myfpds);
+		printf("Updated size: %lu\n", myfpds_size);
 
 
-/* 
-**Create FreePacketDescriptorStore using mem_start and mem_length
-**create any buffers required by our threads
-**create any threads you require for your implementation 
+		myNetworkDevice->awaitIncomingPacket(myNetworkDevice); // thread blocked until populated 
+		// add pd to the bounded buffer and to the array of pd's according to their PID's
+		/*PID pd_pid = getPID(currentPD);
+		arr_pid[num_prog] = pd_pid; 
+		arr_pid[num_prog][0] = currentPD; */
 
-**return the FDPS to the code that called it 
-*/
+		//write packet Descriptor to the bbout buffer once filled by the Network device 
 
-void init_packet_driver(NetworkDevice *nd, void * mem_start, 
+		bbout->blockingWrite(bbout, currentPD);
+
+	}
+	return NULL; 
+}
+
+
+
+PacketDescriptor *currentPDtoSend; 
+void *sendingToAPP(UNUSED void *arg){
+
+	// from here, we need to take the pd in bbout OUT and send it to the corresponding app via PID identifer
+	printf("Thread created successfully\n");
+	while(1){
+
+
+
+		// once packet is sent and finished being used by the app, we then need to put the pd into the bbin??
+		// and THEN pull it out and place into the fpds again???
+	}
+	return NULL; 
+}
+
+void init_packet_driver(NetworkDevice *nd, void *mem_start, 
 	unsigned long mem_length, FreePacketDescriptorStore **fpds){
+	/*
+	**Create FreePacketDescriptorStore using mem_start and mem_length
+	**create any buffers required by our threads
+	**create any threads you require for your implementation 
 
+	**return the FDPS to the code that called it 
+	*/
+
+	myNetworkDevice = nd; 
+	myfpds = FreePacketDescriptorStore_create(mem_start, mem_length); // call the create function for the file descriptor
+	myfpds_size = myfpds->size(myfpds);
+	printf("Size of myfds: %lu\n", myfpds_size);
+
+	bbin = BoundedBuffer_create(1024);
+	bbout = BoundedBuffer_create(1024);
+
+	if((bbin || bbout) == NULL){
+		fprintf(stderr, "Thread creation failed\n");
+
+	*fpds = myfpds;
+	}
+
+	pthread_create(&producer_thread, NULL, sendingToAPP, NULL);
+	pthread_create(&consumer_thread, NULL, getWork, NULL);
+	pthread_join(producer_thread, NULL);
+	pthread_join(consumer_thread, NULL); 
 }
 
 
@@ -80,76 +153,94 @@ void init_packet_driver(NetworkDevice *nd, void * mem_start,
 
 
 
-					/*these two functions hand in a Packet Descriptor for dispatching. Neither call should 
-					delay until the packet is actually sent
-					*/
+/*these two functions hand in a Packet Descriptor for dispatching. Neither call should 
+delay until the packet is actually sent
+*/
 
  
-/*
-Blocking call will usually return promptly, 
-but there may be a delay while it waits for space in the buffers  
 
-**Queue up packet descriptor for sending
-**Do not return until it has be successfullt queued 
-*/
 void blocking_send_packet(PacketDescriptor *pd){
-	return NULL; 
+	/*
+	Blocking call will usually return promptly, 
+	but there may be a delay while it waits for space in the buffers  
 
+	**Queue up packet descriptor for sending
+	**Do not return until it has be successfullt queued 
+	*/
+
+	printf("blocking_send_packet funciton\n");
+	bbin->blockingWrite(bbin, pd);
 }
 
-/*
-Non-blocking call MUST return promptly, 
-indicating whether or not the indicated packet
-has been accepted by my code
 
-**If able to queue up packet descriptor immediately, do so and return 1
-**otherwise return 0
-*/
 int nonblocking_send_packet(PacketDescriptor *pd){
-	return 1; 
+	/*
+	Non-blocking call MUST return promptly, 
+	indicating whether or not the indicated packet
+	has been accepted by my code
+
+	**If able to queue up packet descriptor immediately, do so and return 1
+	**otherwise return 0
+	*/
+	
+
+	/*PID tmp = getPID(pd);
+	arr_pid[tmp];
+	num_prog++;*/
+
+	int success; 
+
+	success = bbin->nonblockingWrite(bbin, pd);
+	currentPD = pd; 
+	if(success){
+		return 1; 
+	}
+	else{
+		return 0; 
+	}
+}
+
+
+/*These two functions represent requests for packets by the application threads
+both calls indicate their process identifier and should only be given appropriate packets 
+Permitted to use a small bounded buffer to hold packets that haven't yet been collected by 
+a process but we are allowed to discard extra packets if at lest one is waiting uncollected 
+for the same PID*/
+
+
+void blocking_get_packet(PacketDescriptor **pd, PID pid){
+	/*
+	Only return when a packet is received 
+	for the indicated process and the first 
+	arg point to it
+	**Wait until there is a packet for 'pid'
+	**return packet descriptor to the calling application
+	*/
+
+	//function that checks if the pid is here
+
+
+	printf("blocking_get_packet() is getting called\n");
+	SandSbuffer((void*)&pd, pid);
+	//return NULL;
+
 }
 
 
 
+int nonblocking_get_packet(PacketDescriptor **pd, PID pid){
+	/*This must return promptly. Result 1
+	 if a packet was found, 0 if no packet 
+	 is waiting
 
+	 **If there is currently a waiting packet for 'pid', return that packet 
+	 	to the calling application and return 1 for the value of the function (hence the int)
+	 	otherwise, return 0 for the value of the function
+	 */
 
-
-
-					/*These two functions represent requests for packets by the application threads
-					both calls indicate their process identifier and should only be given appropriate packets 
-					Permitted to use a small bounded buffer to hold packets that haven't yet been collected by 
-					a process but we are allowed to discard extra packets if at lest one is waiting uncollected 
-					for the same PID*/
-
-/*
-Only return when a packet is received 
-for the indicated process and the first 
-arg point to it
-
-**Wait until there is a packet for 'pid'
-
-**return packet descriptor to the calling application
-*/
-void blocking_get_packet(PacketDescriptor **pd, PID){
-	return NULL; 
-}
-
-
-/*This must return promptly. Result 1
- if a packet was found, 0 if no packet 
- is waiting
-
- **If there is currently a waiting packet for 'pid', return that packet 
- 	to the calling application and return 1 for the value of the function (hence the int)
- 	otherwise, return 0 for the value of the function
- */
-int nonblocking_get_packet(PacketDescriptor **pd, PID){
-
-}
-
-
-
-
-int main(int argc, char *argv[]){
-	return 1;
+	printf("nonblocking_get_packet() is getting called\n");
+	if((SandSbuffer((void*)&pd, pid)) == 1){
+		return 1; 
+	}
+	return 0; 
 }
